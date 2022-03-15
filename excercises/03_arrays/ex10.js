f33d79da920a4ed49314852a16705d9896a8d2bf
Joe Webster <joseph.scott.webster@gmail.com>
2022-03-14 20:19:11 -0400

    Complete Array exercise 10

---
+++ excercises/03_arrays/ex10.js
@@ -0,0 +1,23 @@
+function areArraysEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  
+  let arr1Sorted = arr1.slice().sort();
+  let arr2Sorted = arr2.slice().sort();
+
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr1Sorted[i] !== arr2Sorted[i]) return false;
+  }
+  
+  return true;
+}
+
+console.log(areArraysEqual([1, 2, 3], [1, 2, 3]));                  // true
+console.log(areArraysEqual([1, 2, 3], [3, 2, 1]));                  // true
+console.log(areArraysEqual(['a', 'b', 'c'], ['b', 'c', 'a']));      // true
+console.log(areArraysEqual(['1', 2, 3], [1, 2, 3]));                // false
+console.log(areArraysEqual([1, 1, 2, 3], [3, 1, 2, 1]));            // true
+console.log(areArraysEqual([1, 2, 3, 4], [1, 1, 2, 3]));            // false
+console.log(areArraysEqual([1, 1, 2, 2], [4, 2, 3, 1]));            // false
+console.log(areArraysEqual([1, 1, 2], [1, 2, 2]));                  // false
+console.log(areArraysEqual([1, 1, 1], [1, 1]));                     // false
+console.log(areArraysEqual([1, 1], [1, 1]));                        // true
